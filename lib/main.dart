001ec024c0e358de3c9d08e036cb284a5a7d57e9
KrishnaK <krish2kadu@gmail.com>
2022-07-20 16:32:11 +0530

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,8 @@ class _MyHomePageState extends State<MyHomePage> {
   static final countMatrix = 3;
   static final double size = 96;
 
- late List<List<String>> matrix;
+  String lastMove = Player.none;
+  late List<List<String>> matrix;
   @override
   void initState() {
     super.initState();
@@ -56,9 +57,9 @@ class _MyHomePageState extends State<MyHomePage> {
       );
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context1) {
     return Scaffold(
-      backgroundColor: const Color(0xffd4f6cc),
+      backgroundColor: getBackgroundColor(),
       appBar: AppBar(
         title: Text(widget.title),
       ),
@@ -81,22 +82,96 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
+  Color getFieldColor(String value) {
+    switch (value) {
+      case Player.O:
+        return const Color(0xff3CCF4E);
+      case Player.X:
+        return const Color(0xffEF5B0C);
+      default:
+        return Colors.indigo;
+    }
+  }
+
+  Color getBackgroundColor() {
+    final thisMove = lastMove == Player.X ? Player.O : Player.X;
+
+    return lastMove == Player.none
+        ? const Color(0xffD4F6CC)
+        : thisMove == Player.X
+            ? Color(0xffFF8B8B)
+            : Color(0xff90C8AC);
+  }
+
   Widget buildField(int x, int y) {
     final value = matrix[x][y];
+    final color = getFieldColor(value);
 
     return Container(
       margin: EdgeInsets.all(5),
       child: ElevatedButton(
         style: ElevatedButton.styleFrom(
           minimumSize: Size(size, size),
-          primary: Colors.indigo
+          primary: color,
         ),
-        onPressed: () {},
-        child: Text(
-          value,
-          style: TextStyle(fontSize: 32),
-        ),
+        onPressed: () => selectField(value, x, y),
+        child: Text(value,
+            style: TextStyle(
+              fontSize: 32,
+            )),
       ),
     );
   }
+
+  Future showEndDialog(String title) => showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (context) => AlertDialog(
+          title: Text(title),
+          content: Text('Press to Restart the Game'),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                setEmptyFields();
+                Navigator.of(context).pop();
+              },
+              child: Text('Restart'),
+            ),
+          ],
+        ),
+      );
+
+   bool isWinner(int x, int y) {
+    var col = 0, row = 0, diag = 0, rdiag = 0;
+    final player = matrix[x][y];
+    final n = countMatrix;
+
+    for (int i = 0; i < n; i++) {
+      if (matrix[x][i] == player) col++;
+      if (matrix[i][y] == player) row++;
+      if (matrix[i][i] == player) diag++;
+      if (matrix[i][n - i - 1] == player) rdiag++;
+    }
+
+    return row == n || col == n || diag == n || rdiag == n;
+  }
+
+  bool isEnd() => matrix.every((values) => values.every((value) => value != Player.none));
+
+  void selectField(String value, int x, int y) {
+    if (value == Player.none) {
+      final newValue = lastMove == Player.X ? Player.O : Player.X;
+
+      setState(() {
+        lastMove = newValue;
+        matrix[x][y] = newValue;
+      });
+    if(isWinner(x,y)){
+      showEndDialog('Player $newValue Won ');
+    }
+    else if (isEnd()) {
+        showEndDialog('Undecided Game');
+      }
+  }
+}
 }
